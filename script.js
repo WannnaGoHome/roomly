1e3bb66db3deaaac133beaccd2a6dc364409654c
Delik <iammanhoodboy@gmail.com>
2025-12-01 14:40:07 +0500

    Настраиваю скрытие лишних страниц

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const stateSelectRole = document.getElementById('state-select-role');
 const portals = document.querySelectorAll('.portal');
 const roleButtons = document.querySelectorAll('.role-button[data-role]');
-const switchRoleBtn = document.querySelector('.switch-role-btn');
+const switchRoleBtns = document.querySelectorAll('.switch-role-btn');
 
 function hideAllPortals() {
   portals.forEach((portal) => portal.classList.add('hidden'));
@@ -23,25 +23,27 @@ roleButtons.forEach(btn => {
   });
 });
 
-if (switchRoleBtn) {
-switchRoleBtn.addEventListener('click', showSelectRole);
-}
+switchRoleBtns.forEach(btn => {
+  btn.addEventListener('click', showSelectRole);
+});
 
 const tabButtons = document.querySelectorAll(".student-tabs .tab");
 const tab1 = document.getElementById("tab-schedule");
 const tab2 = document.getElementById("tab-rooms");
 
-tabButtons.forEach((btn, index) => {
-  btn.addEventListener("click", () => {
-    tabButtons.forEach(b => b.classList.remove("active"));
-    btn.classList.add("active");
+if (tabButtons.length > 0) {
+  tabButtons.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      tabButtons.forEach(b => b.classList.remove("active"));
+      btn.classList.add("active");
 
-    if (index === 0) {
-      tab1.classList.remove("hidden");
-      tab2.classList.add("hidden");
-    } else {
-      tab2.classList.remove("hidden");
-      tab1.classList.add("hidden");
-    }
+      if (index === 0) {
+        tab1.classList.remove("hidden");
+        tab2.classList.add("hidden");
+      } else {
+        tab2.classList.remove("hidden");
+        tab1.classList.add("hidden");
+      }
+    });
   });
-});
+}
